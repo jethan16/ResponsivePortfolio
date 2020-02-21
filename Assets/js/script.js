a3e34a9548173ff80b2b33fc43cd93dcb83cf616
Jethan16 <ethan.nel.jones@gmail.com>
2020-02-21 15:58:38 -0500

    added styling and animations

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,11 +2,16 @@ setInterval (function(){
     $("#chevron").toggleClass("chevJump")
 }, 1000)
 
+setInterval (function(){
+    $(".cloud1").toggleClass("cloudWiggle1")
+    $(".cloud2").toggleClass("cloudWiggle2")
+}, 1000)
+
 $(".burger").on("click", function() {
 
-    $("#1").toggleClass("top")
-    $("#2").toggleClass("middle")
-    $("#3").toggleClass("bottom")
+    $("#1").toggleClass("one")
+    $("#2").toggleClass("two")
+    $("#3").toggleClass("three")
     $(".menu").toggleClass("menu-hide")
     
 })
